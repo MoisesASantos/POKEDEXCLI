092b6edcb5767958003e6e22b68b06d586346cf5
MoisesASantos <moisessantosa23@gmail.com>
2026-06-19 16:49:02 +0100

    sending the feature to execute command, with exit and help command

--- main.go
+++ main.go
@@ -6,11 +6,59 @@ import (
 	"bufio"
 	"os"
 	"strings"
+	"json"
+	"net/http"
+	"encoding/json"
 )
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func commandHelp() error {
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println("Usage:\n\n")
+	fmt.Println("help: Displays a help message")
+	fmt.Println("exit: Exit the Pokedex")
+	return nil
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+
+func commandMap() error {
+
+}
+
+commands := map[string]cliCommand{
+	"exit": {
+		name:        "exit",
+		description: "Exit the Pokedex",
+		callback:    commandExit,
+	},
+	"help": {
+		name:        "help",
+		description: "Display a help message",
+		callback:    commandHelp,
+	},
+	"map": {
+		name:        "map",
+		description: "It displays the names of 20 location areas in the Pokemon world",
+		callback:    commandMap,
+	},
+}
 
 func main() {
 	
 	scanner := bufio.NewScanner(os.Stdin)
+
+	
 
 	for {
 		fmt.Print("Pokedex > ")
@@ -20,6 +68,15 @@ func main() {
 		line := scanner.Text()
 		lower_string := strings.ToLower(line)
 		result_list := repl.CleanInput(lower_string)
-		fmt.Printf("Your command was: %s\n", result_list[0])
+		command, exist := commands[result_list[0]]
+		if !exist {
+			fmt.Printf("Unknown command\n")
+			os.Exit(1)
+		}
+
+		err := command.callback()
+		if err != nil {
+			fmt.Println("Issue to execute the command\n")
+		}
 	}
 }
