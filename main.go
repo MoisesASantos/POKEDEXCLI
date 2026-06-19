ac80cf541fa882d64cc0ee7a9507bf5f4a92abca
MoisesASantos <moisessantosa23@gmail.com>
2026-06-19 18:23:12 +0100

    sending the feature to make request on Pokemon API and navegate to the location of pokemon's, add too a map and mapb comand to list the location's

--- main.go
+++ main.go
@@ -1,82 +1,155 @@
 package main
 
 import (
-	"fmt"
-	"github.com/MoisesASantos/POKEDEXCLI/repl"
 	"bufio"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
 	"os"
 	"strings"
-	"json"
-	"net/http"
-	"encoding/json"
+	"time"
+
+	"github.com/MoisesASantos/POKEDEXCLI/repl"
 )
+
+type LocationArea struct {
+	Name string `json:"name"`
+	URL  string `json:"url"`
+}
+
+type config struct {
+	Count       int            `json:"count"`
+	NextURL     *string        `json:"next"`
+	PreviousURL *string        `json:"previous"`
+	Results     []LocationArea `json:"results"`
+}
 
 type cliCommand struct {
 	name        string
 	description string
-	callback    func() error
+	callback    func(*config) error
 }
 
-func commandHelp() error {
+func commandHelp(cfg *config) error {
 	fmt.Println("Welcome to the Pokedex!")
-	fmt.Println("Usage:\n\n")
+	fmt.Println("Usage:\n")
 	fmt.Println("help: Displays a help message")
 	fmt.Println("exit: Exit the Pokedex")
+	fmt.Println("map: Display the next 20 location areas")
+	fmt.Println("mapb: Display the previous 20 location areas")
 	return nil
 }
 
-func commandExit() error {
+func commandExit(cfg *config) error {
 	fmt.Println("Closing the Pokedex... Goodbye!")
 	os.Exit(0)
 	return nil
 }
 
+func makeRequest(url string, cfg *config) error {
+	client := &http.Client{
+		Timeout: 10 * time.Second,
+	}
 
-func commandMap() error {
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return err
+	}
 
+	res, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return err
+	}
+
+	err = json.Unmarshal(body, cfg)
+	if err != nil {
+		return err
+	}
+
+	for _, area := range cfg.Results {
+		fmt.Println(area.Name)
+	}
+
+	return nil
 }
 
-commands := map[string]cliCommand{
-	"exit": {
-		name:        "exit",
-		description: "Exit the Pokedex",
-		callback:    commandExit,
-	},
-	"help": {
-		name:        "help",
-		description: "Display a help message",
-		callback:    commandHelp,
-	},
-	"map": {
-		name:        "map",
-		description: "It displays the names of 20 location areas in the Pokemon world",
-		callback:    commandMap,
-	},
+func commandMap(cfg *config) error {
+	if cfg.NextURL != nil {
+		return makeRequest(*cfg.NextURL, cfg)
+	}
+
+	return makeRequest(
+		"https://pokeapi.co/api/v2/location-area/",
+		cfg,
+	)
+}
+
+func commandMapb(cfg *config) error {
+	if cfg.PreviousURL == nil {
+		fmt.Println("You're on the first page")
+		return nil
+	}
+
+	return makeRequest(*cfg.PreviousURL, cfg)
 }
 
 func main() {
-	
 	scanner := bufio.NewScanner(os.Stdin)
 
-	
+	cfg := &config{}
+
+	commands := map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+		"help": {
+			name:        "help",
+			description: "Display a help message",
+			callback:    commandHelp,
+		},
+		"map": {
+			name:        "map",
+			description: "Display the next 20 location areas",
+			callback:    commandMap,
+		},
+		"mapb": {
+			name:        "mapb",
+			description: "Display the previous 20 location areas",
+			callback:    commandMapb,
+		},
+	}
 
 	for {
 		fmt.Print("Pokedex > ")
+
 		if !scanner.Scan() {
 			break
 		}
+
 		line := scanner.Text()
-		lower_string := strings.ToLower(line)
-		result_list := repl.CleanInput(lower_string)
-		command, exist := commands[result_list[0]]
-		if !exist {
-			fmt.Printf("Unknown command\n")
-			os.Exit(1)
+		resultList := repl.CleanInput(strings.ToLower(line))
+
+		if len(resultList) == 0 {
+			continue
 		}
 
-		err := command.callback()
-		if err != nil {
-			fmt.Println("Issue to execute the command\n")
+		command, exists := commands[resultList[0]]
+		if !exists {
+			fmt.Println("Unknown command")
+			continue
+		}
+
+		if err := command.callback(cfg); err != nil {
+			fmt.Println(err)
 		}
 	}
 }
