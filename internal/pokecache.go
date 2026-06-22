4d31c215fbdbaf1a39461ae2c38efa55cbea9f51
MoisesASantos <moisessantosa23@gmail.com>
2026-06-22 15:28:45 +0100

    it's not profissional, but at this moment I'm seing smoke, but now I add a readLoop function to allow elimite old cache, and NewCache function to allow create ou start the cache struct

--- internal/pokecache.go
+++ internal/pokecache.go
@@ -16,6 +16,12 @@ type cache struct {
 
 func (r cache) Add(key_intro string, data []byte) {
 
+	r.my_mutex.Lock()
+	defer r.my_mutex.Unlock()
+	
+	if r.map_result == nil {
+		r.map_result = make(map[string]cacheEntry)
+	}
 	var entry_to_Add cacheEntry
 
 	entry_to_add.createdAt = time.Now() 
@@ -25,21 +31,47 @@ func (r cache) Add(key_intro string, data []byte) {
 
 func (r cache) Get(key_intro string) ([]byte, bool) {
   
-	result, exits = r.map_result[key_intro]
+	r.my_mutex.Lock()
+	defer r.my_mutex.Unlock()
 
-	if exists != nil {
-		return nil, false
-	}
-	return result.val, true
+	result, ok = r.map_result[key_intro]
+	return result.val, ok
 }
 
-func (r cache) reapLoop(key_intro string) ([]byte, bool) {
+
+func (r cache) remove_item(interval time.Duration) {
+
+	r.my_mutex.Lock()
+	defer r.my_mutex.Unlock()
+
+	limite := time.Now().Add(-interval)
+	for chave, entrada := r.map_result {
+		if entrada.CreatedAt.Before(limete) {
+			delete(r.map_result, chave)
+		}
+	}
+}
+
+func (r cache) reapLoop(interval time.Duration) ([]byte, bool) {
   
-	result, exits = r.map_result[key_intro]
+	//cria um ticker a cada intervalo de tempo, o ticker tem um canal que é enviado um sinal por uma go routine interna cada vez que passa o intervalo de tempo difinido, por cada elemento ou sinal no canal do ticker significa um aviso que passou o intervalo de tempo, sendo assim entra em remove_item, bloquea o map_result de ser acessado por outras go routine, as outras go routine ficam em espera, para ler e alterar o mapa, é calculado o limete de tempo pelo qual o cache se tornou antigo, subtraindo o intervalo do nosso tempo atual, depois fizemos um loop em map_result e o que tiver sido criado antes desse limite é iliminado
+	ticker := time.NewTicker(interval)
+	defer ticker.Stop()
 
-	if exists != nil {
-		return nil, false
+	for range ticker.C {
+		remove_item(interval)
 	}
-	return result.val, true
 }
+
+func NewCache(interval time.Duration) *cache {
+	
+	newCache := cache{
+		map_result: make(map[string]cacheEntry),
+		my_mutex:   new(sync.Mutex),
+	}
+	go newCache.reapLoop(interval)
+	
+	return &newCache
+}
+
 
